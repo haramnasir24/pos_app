4fde140d2b25b1ba2567012c82044458ae0a7e1a
haramnasir24 <haramn518@gmail.com>
2025-06-28 01:33:54 +0500

    updated !!!

--- auth.ts
+++ auth.ts
@@ -6,18 +6,14 @@ import NextAuth from "next-auth";
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [
     {
-      id: "square", // this will be used as signIn("square") and in callback URLs
+      id: "square",
       name: "Square",
-      type: "oauth", // OAuth 2.0 provider
-      // issuer: "https://YOUR_SQUARE_ISSUER_URL",
+      type: "oauth",
       clientId: process.env.SQUARE_CLIENT_ID!,
       clientSecret: process.env.SQUARE_CLIENT_SECRET!,
       authorization: {
         url: "https://connect.squareupsandbox.com/oauth2/authorize",
-        params: {
-          scope: "MERCHANT_PROFILE_READ",
-          response_type: "code",
-        },
+        params: { scope: "MERCHANT_PROFILE_READ", response_type: "code" },
       },
       token: "https://connect.squareupsandbox.com/oauth2/token",
       userinfo: {
@@ -28,7 +24,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
             {
               headers: {
                 Authorization: `Bearer ${tokens.access_token}`,
-                "Square-Version": "2023-10-18", // look this up
+                "Square-Version": "2023-10-18",
               },
             }
           );
@@ -44,47 +40,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
           image: null,
         };
       },
-      checks: ["state"],
     },
   ],
-  secret: process.env.AUTH_SECRET!,
-  trustHost: true,
   debug: true,
-  callbacks: {
-    async session({ session, token }) {
-      return session;
-    },
-    async jwt({ token, account, profile }) {
-      return token;
-    },
-  },
 });
-
-
-// callbacks: {
-//     async session({ session, token }) {
-//       session.user.id = token.sub ?? "";
-//       return session;
-//     },
-//   },
-
-
-// token: {
-//   url: "https://connect.squareupsandbox.com/oauth2/token",
-//   async request({ client, params, checks, provider }) {
-//     const response = await fetch("https://connect.squareupsandbox.com/oauth2/token", {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//         "Square-Version": "2023-10-18", // Use latest Square API version
-//       },
-//       body: JSON.stringify({
-//         client_id: provider.clientId,
-//         client_secret: provider.clientSecret,
-//         code: params.code,
-//         grant_type: "authorization_code",
-//       }),
-//     });
-//     return await response.json();
-//   },
-// },
